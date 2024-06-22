67b0ef5e1f820d07ad1e2e1c408540ccd951d147
Orlando Junior <orlandojsjunior@hotmail.com>
2024-06-22 13:42:53 -0300

    02 - Executando operações CRUD - Incluindo  um registro na tabela.sql

---
+++ 02 - Executando operações CRUD - Incluindo  um registro na tabela.sql
@@ -0,0 +1,27 @@
+INSERT INTO aluno (
+	nome,
+    cpf,
+    observacao,
+    idade,
+    dinheiro,
+    altura,
+    ativo,
+    data_nascimento,
+    hora_aula,
+    matriculado_em
+)
+VALUES (
+	'Orlando',
+	'12345678901',
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fermentum lobortis ornare. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam in dignissim lectus. Suspendisse pellentesque nulla mattis, mollis justo ac, posuere erat. Suspendisse id libero eu quam vestibulum ultrices. Sed eleifend velit enim. Sed ut turpis id lectus elementum laoreet. Duis ultricies nec elit eget porttitor. Praesent eget enim ullamcorper, mattis mi sed, vehicula nunc. Phasellus sodales, neque nec elementum accumsan, massa est vestibulum elit, id placerat elit orci sed massa. Vestibulum ac purus accumsan, iaculis lacus quis, ullamcorper nunc. In dignissim varius risus, et posuere tellus gravida semper. Maecenas sit amet odio tincidunt, luctus nunc convallis, imperdiet augue.',
+	44,
+	100.50,
+	1.82,
+	TRUE,
+	'1979-11-01',
+	'17:30:00',
+	'2024-06-22 13:01:00'
+);
+
+SELECT * FROM aluno;
+

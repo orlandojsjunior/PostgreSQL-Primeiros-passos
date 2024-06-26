4544114a0dac310d867f6359520d55ab1d3591ee
Orlando Junior <orlandojsjunior@hotmail.com>
2024-06-26 20:34:39 -0300

    Criando uma tabela e renomeando uma coluna

---
+++ Aula01-atividade03 - Criando uma tabela e renomeando uma coluna.sql
@@ -0,0 +1,24 @@
+CREATE TABLE aluno
+(
+	id SERIAL,
+	nome VARCHAR(255),
+	cpf CHAR(11),
+	observacao TEXT,
+	idade INTEGER,
+	dinehiro NUMERIC(10,2),
+	altura REAL,
+	ativo BOOLEAN,
+	data_nascimento DATE,
+	hora_aula TIME,
+	matriculado_em TIMESTAMP
+	
+);
+
+select * from aluno;
+
+'''
+Corrigigindo a coluna dinehiro
+
+'''
+
+ALTER TABLE aluno RENAME COLUMN dinehiro TO dinheiro;

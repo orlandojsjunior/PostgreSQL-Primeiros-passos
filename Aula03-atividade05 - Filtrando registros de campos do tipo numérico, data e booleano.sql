690002de5dbbae829291a302d18f9c6dfd359d3d
Orlando Junior <orlandojsjunior@hotmail.com>
2024-06-27 19:38:26 -0300

    Update - Aula03-atividade05 - Filtrando registros de campos do tipo numérico, data e booleano
    Renomeando arquivo

--- Aula03-atividade05 - Filtrando registros de campos do tipo numérico, data e booleano.sql
+++ Aula03-atividade05 - Filtrando registros de campos do tipo numérico, data e booleano.sql
@@ -1,4 +1,4 @@
-'''
+/*
 Nesta aula aprenderemos os filtros para os outros campos, mas primeiro 
 aprenderemos dois modelos que funcionam para qualquer tipo de campo, incluindo os de texto, o IS e IS NOT.
 
@@ -7,7 +7,8 @@ Ao selecionarmos a nossa tabela com SELECT * FROM aluno; ,
 notamos que apenas o CPF do "Diogo" está preenchido, enquanto os demais estão nulos.
 Para filtrar esses dados, não podemos usar WHERE cpf = NULL , porque "= NULL" não é um resultado válido, 
 então não retornará nenhum resultado.
-'''
+*/
+
 SELECT *
     FROM aluno
  WHERE cpf IS NULL;
@@ -54,9 +55,8 @@ SELECT *
 
 
 
-'''
-Filtros Booleano
-'''
+-- Filtros Booleano
+
 SELECT * FROM aluno WHERE ativo = false
 
 SELECT * FROM aluno WHERE ativo = true

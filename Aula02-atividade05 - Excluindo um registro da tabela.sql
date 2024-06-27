3e2af82e7317ce3f30a1f87bed69cb471d42c61e
Orlando Junior <orlandojsjunior@hotmail.com>
2024-06-26 23:26:47 -0300

    Create Aula02-atividade05 - Excluindo um registro da tabela.sql

---
+++ Aula02-atividade05 - Excluindo um registro da tabela.sql
@@ -0,0 +1,17 @@
+ SELECT * 
+   FROM aluno
+  WHERE nome = 'Nico';
+
+DELETE 
+   FROM aluno
+  WHERE nome = 'Nico';
+
+
+
+ SELECT * 
+   FROM aluno
+  WHERE nome = 'Orlando';
+
+DELETE 
+   FROM aluno
+  WHERE nome = 'Orlando';

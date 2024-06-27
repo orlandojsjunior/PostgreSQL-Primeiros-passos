5746166f8a86af7485660a4d2aa84e4331d6938c
Orlando Junior <orlandojsjunior@hotmail.com>
2024-06-27 19:39:51 -0300

    Update - Aula03-atividade07 - Filtrando utilizando operadores condicionais AND e OR.sql

--- Aula03-atividade07 - Filtrando utilizando operadores condicionais AND e OR.sql
+++ Aula03-atividade07 - Filtrando utilizando operadores condicionais AND e OR.sql
@@ -1,10 +1,10 @@
-'''
+/*
 Nesta aula aprenderemos como utilizar os operadores AND (e) e OR (ou) para combinar os filtros. 
 Começaremos pelo operador AND .
 Imaginemos a situação em que precisássemos resgatar todos os nomes que comecem com a letra "D" e tenham o CPF preenchido.
 Para isso, usamos o operador AND , que funciona como uma soma de condições, 
 apresentando apenas os resultados que atendam às duas exigências que colocamos.
-'''
+*/
 
 SELECT
 	*

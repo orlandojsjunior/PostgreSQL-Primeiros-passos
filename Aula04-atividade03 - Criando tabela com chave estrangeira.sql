260d01337a361e4effac9884e52ac6ddbf620dc5
Orlando Junior <orlandojsjunior@hotmail.com>
2024-06-27 19:12:28 -0300

    Corrigindo a identação da  Aula04-atividade03 - Criando tabela com chave estrangeira

--- Aula04-atividade03 - Criando tabela com chave estrangeira.sql
+++ Aula04-atividade03 - Criando tabela com chave estrangeira.sql
@@ -1,74 +1,162 @@
-SELECT * FROM aluno;
-SELECT * FROM curso;
+SELECT
+    *
+FROM
+    ALUNO;
 
-DROP TABLE aluno;
-	
+SELECT
+    *
+FROM
+    CURSO;
 
-CREATE TABLE aluno (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL
-); 
+DROP TABLE ALUNO;
 
-INSERT INTO aluno (nome) VALUES ('Diogo');
-INSERT INTO aluno (nome) VALUES ('Vinícius');
+CREATE TABLE ALUNO (
+    ID SERIAL PRIMARY KEY,
+    NOME VARCHAR(255) NOT NULL
+);
+
+INSERT INTO ALUNO (
+    NOME
+) VALUES (
+    'Diogo'
+);
+
+INSERT INTO ALUNO (
+    NOME
+) VALUES (
+    'Vinícius'
+);
 
 -- Temos uma tabela com dois alunos e outra tabela com dois cursos. Para relacionarmos os dois bancos de dados, criaremos uma tabela nova.
 
-CREATE TABLE aluno_curso (
-    aluno_id INTEGER,
-    curso_id INTEGER,
-    PRIMARY KEY (aluno_id, curso_id)
-); -- Relacionando as tabelas
+CREATE TABLE ALUNO_CURSO (
+    ALUNO_ID INTEGER,
+    CURSO_ID INTEGER,
+    PRIMARY KEY (ALUNO_ID, CURSO_ID)
+);
 
-SELECT * FROM aluno;
-SELECT * FROM curso; 
+-- Relacionando as tabelas
+
+SELECT
+    *
+FROM
+    ALUNO;
+
+SELECT
+    *
+FROM
+    CURSO;
 
 '''
 Ao criarmos nossa tabela, codamos uma chave primária com dois campos, ou seja, uma chave primária composta. 
 Dessa forma, declaramos que tanto o id do aluno, quanto o id do curso é um campo único que não pode ser nulo.
 '''
 
-SELECT * FROM aluno_curso;
+SELECT
+    *
+FROM
+    ALUNO_CURSO;
 
-INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (1,1);
-INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (2,1);
+INSERT INTO ALUNO_CURSO (
+    ALUNO_ID,
+    CURSO_ID
+) VALUES (
+    1,
+    1
+);
 
-SELECT * FROM aluno WHERE id = 1;
-SELECT * FROM curso WHERE id = 1;
+INSERT INTO ALUNO_CURSO (
+    ALUNO_ID,
+    CURSO_ID
+) VALUES (
+    2,
+    1
+);
 
-SELECT * FROM aluno WHERE id = 2;
-SELECT * FROM curso WHERE id = 1;
+SELECT
+    *
+FROM
+    ALUNO
+WHERE
+    ID = 1;
 
-INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (3,1); -- Inserindo um aluno que nao existe na tabela, (Query returned successfully), nesse caso cria uma inconsistência no BD por causa da auxencia de chave estrangeira. 
+SELECT
+    *
+FROM
+    CURSO
+WHERE
+    ID = 1;
+
+SELECT
+    *
+FROM
+    ALUNO
+WHERE
+    ID = 2;
+
+SELECT
+    *
+FROM
+    CURSO
+WHERE
+    ID = 1;
+
+INSERT INTO ALUNO_CURSO (
+    ALUNO_ID,
+    CURSO_ID
+) VALUES (
+    3,
+    1
+);
+
+-- Inserindo um aluno que nao existe na tabela, (Query returned successfully), nesse caso cria uma inconsistência no BD por causa da auxencia de chave estrangeira.
 
 
-DROP TABLE aluno_curso;
+DROP TABLE ALUNO_CURSO;
 
 -- Criando a tabela com as chave primaria e estrangeira e referenciando os campos nas tabelas.
-	
-CREATE TABLE aluno_curso (
-    aluno_id INTEGER,
-        curso_id INTEGER,
-        PRIMARY KEY (aluno_id, curso_id),
 
-        FOREIGN KEY (aluno_id) -- CAMPO_NA_TABELA_ORIGEM
-         REFERENCES aluno (id), -- CAMPO_NA_TABELA DESTINO
+CREATE TABLE ALUNO_CURSO (
+    ALUNO_ID INTEGER,
+    CURSO_ID INTEGER,
+    PRIMARY KEY (ALUNO_ID, CURSO_ID),
+    FOREIGN KEY (ALUNO_ID) -- CAMPO_NA_TABELA_ORIGEM
+    REFERENCES ALUNO (ID), -- CAMPO_NA_TABELA DESTINO
+    FOREIGN KEY (CURSO_ID) -- CAMPO_NA_TABELA_ORIGEM
+    REFERENCES CURSO (ID) -- CAMPO_NA_TABELA DESTINO
+);
 
-        FOREIGN KEY (curso_id) -- CAMPO_NA_TABELA_ORIGEM
-         REFERENCES curso (id) -- CAMPO_NA_TABELA DESTINO
+INSERT INTO ALUNO_CURSO (
+    ALUNO_ID,
+    CURSO_ID
+) VALUES (
+    1,
+    1
+);
 
-); 
+INSERT INTO ALUNO_CURSO (
+    ALUNO_ID,
+    CURSO_ID
+) VALUES (
+    2,
+    1
+);
 
-INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (1,1);
-INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (2,1);
+INSERT INTO ALUNO_CURSO (
+    ALUNO_ID,
+    CURSO_ID
+) VALUES (
+    3,
+    1
+);
 
-INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (3,1); -- Agora a tabela não aceita a inclusao de registros de um aluno ou curso que nao existe no BD.
-INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (3,3);
-
+-- Agora a tabela não aceita a inclusao de registros de um aluno ou curso que nao existe no BD.
+INSERT INTO ALUNO_CURSO (
+    ALUNO_ID,
+    CURSO_ID
+) VALUES (
+    3,
+    3
+);
 
 -- Obs.: as chaves desempenham um papel crucial na prevenção de inconsistências em bancos de dados (BD).
-
-
-
-
-

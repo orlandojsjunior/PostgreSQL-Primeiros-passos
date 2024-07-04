0e754e20ae1b47bb3542ee6873fe71d47b8c3855
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-04 20:05:26 -0300

    LEFT, RIGHT, CROSS e FULL JOINS

---
+++ Aula04-Atividade07 - LEFT, RIGHT, CROSS e FULL JOINS.sql
@@ -0,0 +1,82 @@
+/*
+Imaginemos a situação em que um aluno não está matriculado em nenhum curso. 
+Vamos aprender uma forma de encontrá-lo na nossa pesquisa. Antes disso, 
+registraremos esse aluno na nossa tabela com INSERT INTO aluno (nome) VALUES ('Nico'); . 
+Ao escrevermos SELECT * FROM aluno , encontramos o registro do Nico na tabela "aluno", 
+porém isso muda ao executarmos o código que retorna alunos e cursos.
+*/
+
+INSERT INTO aluno (nome) VALUES ('Nico');
+
+SELECT aluno.nome as "Nome do Aluno",
+        curso.nome as "Nome do Curso"
+    FROM aluno
+    JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+    JOIN curso ON curso.id = aluno_curso.curso_id
+
+/*
+O registro do Nico não aparece nos resultados porque ele não está matriculado em nenhum curso. 
+O mesmo acontece se escrevermos INSERT INTO curso (id, nome) VALUES (3, 'CSS') , 
+que aparece na tabela "curso", mas não aparece na busca acima.
+
+Isso acontece porque o comando JOIN só retorna os dados de alunos matriculados, 
+o que não é o caso do Nico, e de cursos com alunos, o que não é o caso do CSS. 
+Portanto, ao fazer a busca do "id = 3" na tabela aluno_curso, 
+o JOIN não encontra correspondência e descarta essa informação dos resultados.
+
+Por isso aprenderemos comandos para juntar tabelas quando há informações faltando. 
+O primeiro comando é o LEFT JOIN , que significa que existe um dado na tabela da esquerda, 
+mas não existe na tabela da direita.
+*/
+
+INSERT INTO curso (id, nome) VALUES (3, 'CSS');
+
+
+-- JUNTANDO E PRIORIZANDO OS DADOS DA TABELA DA ESQUERDA 
+SELECT aluno.nome as "Nome do Aluno",
+        curso.nome as "Nome do Curso"
+    FROM aluno
+LEFT JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+LEFT JOIN curso       ON curso.id             = aluno_curso.curso_id
+
+	
+-- JUNTANDO E PRIORIZANDO OS DADOS DA TABELA DA DIREITA	
+SELECT aluno.nome as "Nome do Aluno",
+        curso.nome as "Nome do Curso"
+    FROM aluno
+RIGHT JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+RIGHT JOIN curso       ON curso.id             = aluno_curso.curso_id
+
+
+
+/*
+A terceira situação é quando não importa se o dado está apenas na tabela da direita ou apenas na tabela da esquerda, 
+queremos que ele apareça. Para isso usamos o código FULL JOIN , que irá considerar todos os dados, mesmo que o campo da direita ou o campo da esquerda esteja nulo.
+*/
+	
+SELECT aluno.nome as "Nome do Aluno",
+       curso.nome as "Nome do Curso"
+    FROM aluno
+FULL JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+FULL JOIN curso       ON curso.id             = aluno_curso.curso_id
+
+
+
+/*
+Adicionaremos mais um aluno para descobrirmos o que acontece com o comando CROSS JOIN . 
+Então codamos INSERT INTO aluno (nome) VALUES ('João'); . Após registramos o João, podemos executar novamente nosso último select.
+	
+Existe outro tipo de junção que relaciona todos os dados da tabela "A" com todos os dados da tabela "B". 
+Para essa junção, escreveremos o select sem passar pela tabela "aluno_curso".
+*/
+
+INSERT INTO aluno (nome) VALUES ('João');
+
+SELECT aluno.nome as "Nome do Aluno",
+       curso.nome as "Nome do Curso"
+    FROM aluno
+CROSS JOIN curso
+
+
+
+

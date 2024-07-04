982477fc35afc387cdcebacee0914508e606d9eb
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-04 16:02:17 -0300

    UPDATE - Consultas com relacionamentos

--- Aula04-Atividade05 - Consultas com relacionamentos.sql
+++ Aula04-Atividade05 - Consultas com relacionamentos.sql
@@ -8,15 +8,23 @@ SELECT *
   JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
 
 
+SELECT *
+  FROM aluno
+  JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+  JOIN curso       ON curso.id             = aluno_curso.curso_id
+
+
+	
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (2,2);
+
 
 SELECT aluno.nome as aluno,
        curso.nome as curso
   FROM aluno
   JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
-  JOIN curso ON curso.id = aluno_curso.curso_id
+  JOIN curso       ON curso.id             = aluno_curso.curso_id
 
 
-INSERT INTO aluno_curso
 
 
 SELECT aluno.nome as "Nome do Aluno",

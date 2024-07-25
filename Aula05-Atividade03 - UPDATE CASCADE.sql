333e5b10101dd7c0c0ac5aff24030789747b6f93
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-25 20:48:42 -0300

    Update - UPDATE CASCADE.sql
    Corrigindo indentação

--- Aula05-Atividade03 - UPDATE CASCADE.sql
+++ Aula05-Atividade03 - UPDATE CASCADE.sql
@@ -1,8 +1,10 @@
 -- Função UPDATE
 
-UPDATE aluno
-    SET id = 10
-    WHERE id = 2;
+UPDATE ALUNO
+SET
+    ID = 10
+WHERE
+    ID = 2;
 
 /*
 Nosso código solicitou ao programa a atualização do "id = 2" para o "id = 10", 
@@ -10,52 +12,65 @@ mas a execução retorna uma mensagem de erro. Isso acontece pelo mesmo motivo q
 não conseguimos atualizar os dados de uma tabela relacionada a outro banco de dados. Tanto que se tentarmos atualizar o id do João, 
 que não está matriculado em nenhum curso, teremos sucesso.*/
 
-UPDATE aluno
-    SET id = 20
-    WHERE id = 4;
+UPDATE ALUNO
+SET
+    ID = 20
+WHERE
+    ID = 4;
 
 -- Assim como atualizamos com o comando DELETE CASCADE, adicionaremos o código UPDATE CASCADE
 
-DROP TABLE aluno_curso; -- apagando a tebala para atulizar o código
+DROP TABLE ALUNO_CURSO;
+
+-- apagando a tebala para atulizar o código
 
 -- Se eu altero um dado na tabela de aluno vai ser feito um UPDATE tbm na tabela aluno_curso
 
-CREATE TABLE aluno_curso (
-    aluno_id INTEGER,
-        curso_id INTEGER,
-        PRIMARY KEY (aluno_id, curso_id),
-
-        FOREIGN KEY (aluno_id)
-         REFERENCES aluno (id)
-         ON DELETE CASCADE
-         ON UPDATE CASCADE,
-
-        FOREIGN KEY (curso_id)
-         REFERENCES curso (id)
-
+CREATE TABLE ALUNO_CURSO (
+    ALUNO_ID INTEGER,
+    CURSO_ID INTEGER,
+    PRIMARY KEY (ALUNO_ID, CURSO_ID),
+    FOREIGN KEY (ALUNO_ID) REFERENCES ALUNO (ID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (CURSO_ID) REFERENCES CURSO (ID)
 );
 
 /* Com nosso novo código, quando atualizarmos a tabela "aluno", o banco de dados "aluno_curso" também terá mudanças. 
 Para testarmos, faremos as matrículas novamente, tomando cuidado para não incluirmos o Diogo, já que ele não está mais registrado.*/
 
-INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (2,1);
-INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (3,1);
+INSERT INTO ALUNO_CURSO (
+    ALUNO_ID,
+    CURSO_ID
+) VALUES (
+    2,
+    1
+);
 
+INSERT INTO ALUNO_CURSO (
+    ALUNO_ID,
+    CURSO_ID
+) VALUES (
+    3,
+    1
+);
 
 /* Executando a query, confirmaremos que os alunos Vinícius e Nico foram matriculados no cuso de HTML. 
 Adicionando aluno.id as aluno_id, e curso.id as curso_id à query, também veremos os id's dos alunos e dos cursos, o que facilitará nosso teste.*/
 
 
-SELECT 
-        aluno.id as aluno_id,
-        aluno.nome as "Nome do Aluno",
-        curso.id as "curso_id",
-        curso.nome as "Nome do Curso"
-    FROM aluno
-    JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
-    JOIN curso ON curso.id = aluno_curso.curso_id
-
-
-UPDATE aluno SET id = 10 WHERE id = 2 -- atualizando a tabela
-	
-SELECT * FROM aluno_curso;
+SELECT
+    ALUNO.ID AS ALUNO_ID,
+    ALUNO.NOME AS "Nome do Aluno",
+    CURSO.ID AS "curso_id",
+    CURSO.NOME AS "Nome do Curso"
+FROM
+    ALUNO
+    JOIN ALUNO_CURSO
+    ON ALUNO_CURSO.ALUNO_ID = ALUNO.ID
+    JOIN CURSO
+    ON CURSO.ID = ALUNO_CURSO.CURSO_ID UPDATE ALUNO SET ID = 10
+WHERE
+    ID = 2 -- atualizando a tabela
+    SELECT
+        *
+    FROM
+        ALUNO_CURSO;

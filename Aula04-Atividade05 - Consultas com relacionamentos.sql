c44fb0275343474541ca2d322c35a77c07f321d2
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-27 19:32:42 -0300

    update -  Consultas com relacionamentos

--- Aula04-Atividade05 - Consultas com relacionamentos.sql
+++ Aula04-Atividade05 - Consultas com relacionamentos.sql
@@ -1,22 +1,29 @@
-SELECT * FROM aluno;
-SELECT * FROM curso;
+SELECT
+  *
+FROM
+  ALUNO;
 
+SELECT
+  *
+FROM
+  CURSO;
 
-
-SELECT *
-  FROM aluno
-  JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
-
-
-SELECT *
-  FROM aluno
-  JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
-  JOIN curso       ON curso.id             = aluno_curso.curso_id
-
-
-	
-INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (2,2);
-
+SELECT
+  *
+FROM
+  ALUNO
+  JOIN ALUNO_CURSO
+  ON ALUNO_CURSO.ALUNO_ID = ALUNO.ID
+  SELECT
+    *
+  FROM
+    ALUNO
+    JOIN ALUNO_CURSO
+    ON ALUNO_CURSO.ALUNO_ID = ALUNO.ID
+    JOIN CURSO
+    ON CURSO.ID = ALUNO_CURSO.CURSO_ID INSERT INTO ALUNO_CURSO (ALUNO_ID,
+    CURSO_ID) VALUES (2,
+    2);
 
 SELECT aluno.nome as aluno,
        curso.nome as curso
@@ -46,4 +53,4 @@ Até o momento, para sabermos em qual o curso o aluno está matriculado,
 usamos o SELECT * FROM nome_da_tabela WHERE id = para consultar individualmente o id do aluno e do curso em cada tabela. 
 Contudo, essa não é uma boa forma para visualizarmos os dados, então aprenderemos como fazer essa consulta em uma única query.
 
-Para isso, faremos uma junção (JOIN), comando que une os dados existentes na tabela "A" com os dados existentes na tabela "B". 
+Para isso, faremos uma junção (JOIN), comando que une os dados existentes na tabela "A" com os dados existentes na tabela "B".
